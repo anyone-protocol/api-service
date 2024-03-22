7609a80aafe3de7ee03e15d96d20ec103850aa89
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-03-22 18:25:42 +0200

    ATOR-193 - Add logging to request and response

--- src/VictoriaMetricsService.ts
+++ src/VictoriaMetricsService.ts
@@ -16,3 +16,15 @@ export class VictoriaMetricsService {
         return response.data;
     }
 }
+
+// Request interceptor
+axios.interceptors.request.use(request => {
+    console.log('Starting Request', JSON.stringify(request, null, 2));
+    return request;
+});
+
+// Response interceptor
+axios.interceptors.response.use(response => {
+    console.log('Response:', JSON.stringify(response, null, 2));
+    return response;
+});
