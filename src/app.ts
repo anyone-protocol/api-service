2307900534d1d9c1313d5b528d43ac969f79ce28
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-03-22 16:45:00 +0200

    ATOR-193 - Fix npm build

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { VictoriaMetricsService } from './victoriaMetricsService';
+import { VictoriaMetricsService } from './VictoriaMetricsService';
 import dotenv from 'dotenv';
 dotenv.config();
 
