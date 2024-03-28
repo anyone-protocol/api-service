e82322c3e8153402a0dfb05b73eec72a49c5aba0
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-03-28 09:35:41 +0200

    ATOR-193 - Extract common code to separate function

--- src/app.ts
+++ src/app.ts
@@ -9,74 +9,28 @@ const port = 3000;
 const vmService = new VictoriaMetricsService(process.env.VICTORIA_METRICS_ADDRESS as string);
 
 app.get('/metrics/:query', async (req, res) => {
-    try {
-        const data = await vmService.query(req.params.query);
-        console.log(data);
-
-        // Transform the response
-        // @ts-ignore
-        const transformedResponse = data.data.result.reduce((acc, item) => {
-            acc[item.metric.status] = item.value[1];
-            return acc;
-        }, {});
-        console.log(transformedResponse);
-
-        res.json(transformedResponse);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error querying VictoriaMetrics');
-    }
+    await handleQuery(req.params.query, res);
 });
 
-//total_relays
 app.get('/total-relays', async (req, res) => {
-    try {
-        const data = await vmService.query("total_relays{cluster=\"local\", env=\"main\", instance=\"10.1.244.1:9190\", job=\"consulagentonionoo\"}");
-        console.log(data);
-
-        // Transform the response
-        // @ts-ignore
-        const transformedResponse = data.data.result.reduce((acc, item) => {
-            acc[item.metric.status] = item.value[1];
-            return acc;
-        }, {});
-        console.log(transformedResponse);
-
-        res.json(transformedResponse);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error querying VictoriaMetrics');
-    }
+    await handleQuery("total_relays{cluster=\"local\", env=\"main\", instance=\"10.1.244.1:9190\", job=\"consulagentonionoo\"}", res);
 });
 
 app.get('/total-observed-bandwidth', async (req, res) => {
-    try {
-        const data = await vmService.query("total_observed_bandwidth{cluster=\"local\", env=\"main\", instance=\"10.1.244.1:9190\", job=\"consulagentonionoo\"}");
-        console.log(data);
-
-        // Transform the response
-        // @ts-ignore
-        const transformedResponse = data.data.result.reduce((acc, item) => {
-            acc[item.metric.status] = item.value[1];
-            return acc;
-        }, {});
-        console.log(transformedResponse);
-
-        res.json(transformedResponse);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error querying VictoriaMetrics');
-    }
+    await handleQuery("total_observed_bandwidth{cluster=\"local\", env=\"main\", instance=\"10.1.244.1:9190\", job=\"consulagentonionoo\"}", res);
 });
 
 app.get('/average-bandwidth-rate', async (req, res) => {
+    await handleQuery("average_bandwidth_rate{cluster=\"local\", env=\"main\", instance=\"10.1.244.1:9190\", job=\"consulagentonionoo\"}", res);
+});
+
+async function handleQuery(queryString: string, res: any) {
     try {
-        const data = await vmService.query("average_bandwidth_rate{cluster=\"local\", env=\"main\", instance=\"10.1.244.1:9190\", job=\"consulagentonionoo\"}");
+        const data = await vmService.query(queryString);
         console.log(data);
 
         // Transform the response
-        // @ts-ignore
-        const transformedResponse = data.data.result.reduce((acc, item) => {
+        const transformedResponse = data.data.result.reduce((acc: any, item: any) => {
             acc[item.metric.status] = item.value[1];
             return acc;
         }, {});
@@ -87,7 +41,7 @@ app.get('/average-bandwidth-rate', async (req, res) => {
         console.error(error);
         res.status(500).send('Error querying VictoriaMetrics');
     }
-});
+}
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
