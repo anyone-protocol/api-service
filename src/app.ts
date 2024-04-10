7fde6258fd2037152cbb42940a37b4574bd1fd63
Yurii Kovalchuk <103324562+yumirkov@users.noreply.github.com>
2024-04-10 11:01:32 +0300

    ATOR-232 - Add latest endpoints (#4)

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import QueryString from 'qs';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT ?? 3000;
 const vmService = new VictoriaMetricsService(process.env.VICTORIA_METRICS_ADDRESS as string);
 
 const CLUSTER = process.env.CLUSTER ?? 'local';
@@ -17,27 +17,57 @@ const FROM = process.env.FROM ?? '-7d';
 const TO = process.env.TO ?? 'now';
 const INTERVAL = process.env.INTERVAL ?? '6h';
 
-app.get('/query-range/:query', async (req, res) => {
-    await handleQuery(req.params.query, req.query, res);
-});
+const TOTAL_RELAYS_METRIC = 'total_relays';
+const TOTAL_OBSERVED_BANDWIDTH_METRIC = 'total_observed_bandwidth';
+const AVERAGE_BANDWIDTH_RATE_METRIC = 'average_bandwidth_rate';
 
 app.get('/total-relays', async (req, res) => {
-    await handleQuery(buildQuery("total_relays"), req.query, res);
+    await handleQueryRange(buildQuery(TOTAL_RELAYS_METRIC), req.query, res);
 });
 
 app.get('/total-observed-bandwidth', async (req, res) => {
-    await handleQuery(buildQuery("total_observed_bandwidth"), req.query, res);
+    await handleQueryRange(buildQuery(TOTAL_OBSERVED_BANDWIDTH_METRIC), req.query, res);
 });
 
 app.get('/average-bandwidth-rate', async (req, res) => {
-    await handleQuery(buildQuery("average_bandwidth_rate"), req.query, res);
+    await handleQueryRange(buildQuery(AVERAGE_BANDWIDTH_RATE_METRIC), req.query, res);
+});
+
+app.get('/total-relays-latest', async (req, res) => {
+    await handleQuery(buildQuery(TOTAL_RELAYS_METRIC), res);
+});
+
+app.get('/total-observed-bandwidth-latest', async (req, res) => {
+    await handleQuery(buildQuery(TOTAL_OBSERVED_BANDWIDTH_METRIC), res);
+});
+
+app.get('/average-bandwidth-rate-latest', async (req, res) => {
+    await handleQuery(buildQuery(AVERAGE_BANDWIDTH_RATE_METRIC), res);
 });
 
 function buildQuery(metric: string): string {
     return `${metric}{cluster="${CLUSTER}", env="${ENV}", instance="${ONIONOO_INSTANCE}", job="${JOB}"}`;
 }
 
-async function handleQuery(query: string, params: QueryString.ParsedQs, res: any) {
+async function handleQuery(query: string, res: any) {
+    try {
+        const vmRawData = await vmService.query(query);
+        console.log('VM RAW DATA:', vmRawData);
+
+        const mappedData = vmRawData.data.result.reduce((acc: any, item: any) => {
+            acc[item.metric.status] = item.value[1];
+            return acc;
+        }, {});
+        console.log('MAPPED DATA:', mappedData);
+
+        res.json(mappedData);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error querying VictoriaMetrics');
+    }
+}
+
+async function handleQueryRange(query: string, params: QueryString.ParsedQs, res: any) {
     try {
         const from = String(params.from ?? FROM);
         const to = String(params.to ?? TO);
@@ -59,6 +89,8 @@ async function handleQuery(query: string, params: QueryString.ParsedQs, res: any
     }
 }
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
+
+export { app };
