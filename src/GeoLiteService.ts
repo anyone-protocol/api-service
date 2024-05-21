654816693d5ed1381d5f2b4544abc6b8efd665e1
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-05-21 10:47:01 +0300

    Fix after review

--- src/GeoLiteService.ts
+++ src/GeoLiteService.ts
@@ -2,7 +2,6 @@ import * as geoip from 'geoip-lite';
 
 export class GeoLiteService {
 
-    // https://github.com/geoip-lite/node-geoip
     ipToGeo(ip: string): number[]|null {
         return geoip.lookup(ip)?.ll?? null;
     } 
