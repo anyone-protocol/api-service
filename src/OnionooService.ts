e990a5022eb355c7f33cab6d901c6f8cd6cef920
Jim Toth <jim@artby.city>
2025-08-19 15:39:40 -0400

    Update OnionooService.ts
    Signed-off-by: Jim Toth <jim@artby.city>

--- src/OnionooService.ts
+++ src/OnionooService.ts
@@ -13,9 +13,9 @@ export class OnionooService {
     }
 
     async updateHardwareInfo(hardwareInfo: HardwareInfo): Promise<any> {
-        const fingerprint = encodeURIComponent(hardwareInfo.fingerprint);
+        const fingerprint = hardwareInfo.fingerprint
         if (fingerprint.length != 40) { throw new Error('Fingerprint must be 40 char hex string') }
-        const response = await axios.put(`${this.baseUrl}/hardware/${fingerprint}`, hardwareInfo).then();
+        const response = await axios.put(`${this.baseUrl}/hardware/${encodeURIComponent(fingerprint)}`, hardwareInfo).then();
         return response.data;
     }
 }
